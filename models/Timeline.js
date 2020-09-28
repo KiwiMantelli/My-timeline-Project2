7a500d1756f0e3e8b66e743646ecc459b1c40414
KiwiMantelli <marina.13@free>
2020-09-28 09:46:01 +0200

    models et views

--- models/Timeline.js
+++ models/Timeline.js
@@ -3,11 +3,9 @@ const Schema = mongoose.Schema;
     
     const timelineSchema = new Schema({
         name: String,
-        date: String,
-        description: String,
         category: {type: String, enum: ["general", "trips", "books/movies/series", "family"] },
-        id_tags: { type: Schema.Types.ObjectId, ref: "Tag" },
-        image: String,
+        events: { type: Schema.Types.ObjectId, ref: "Event" },
+       
     
     });
     const Timeline = mongoose.model("Timeline", timelineSchema);
