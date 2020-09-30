0953e112a5d8b78bbdb4a71e96af4b094661eefa
KiwiMantelli <marina.13@free>
2020-09-30 14:17:47 +0200

    css and routes

--- routes/event.js
+++ routes/event.js
@@ -4,38 +4,42 @@ const Event = require("../models/Event");
 const uploader = require("../config/cloudinary");
 
 router.get("/:timelineId/event/create", (req, res, next) => {
-const timelineId = req.params.timelineId;
-const cat= req.params.category;
-const data={};
-console.log(data)
-console.log(cat)
-if(cat ==="trips") data.isTrips = true;
-if(cat === "culture") data.isBooks = true;
-if(cat === "family") data.isFamily = true;
+  const timelineId = req.params.timelineId;
+  const cat = req.params.category;
+  const data = {};
+  console.log(data);
+  console.log(cat);
+  if (cat === "trips") data.isTrips = true;
+  if (cat === "culture") data.isBooks = true;
+  if (cat === "family") data.isFamily = true;
   res.render("create-event", { title: "Create New Event", data, timelineId });
 });
 
-router.post("/:timelineId/event/create", uploader.single("image"), async (req, res, next) => {
-  const newEvents = req.body;
-  newEvents.user_id = req.session.currentUser._id;
-  newEvents.timeline_id = req.params.timelineId;
-  if (req.file) {
-    newEvents.image = req.file.path;
+router.post(
+  "/:timelineId/event/create",
+  uploader.single("image"),
+  async (req, res, next) => {
+    const newEvents = req.body;
+    newEvents.user_id = req.session.currentUser._id;
+    newEvents.timeline_id = req.params.timelineId;
+    if (req.file) {
+      newEvents.image = req.file.path;
+    }
+    try {
+      const newEvent = await Event.create(newEvents);
+      res.redirect(`/timeline/${newEvents.timeline_id}/display`);
+    } catch (error) {
+      next(error);
+    }
   }
-  try {
-    const newEvent = await Event.create(newEvents);
-    res.redirect(`/timeline/${newEvents.timeline_id}/display`);
-  } catch (error) {
-    next(error);
-  }
-});
+);
 
 router.get("/event/:id/edit", async (req, res, next) => {
   try {
     const eventId = req.params.id;
-    console.log(eventId)
+    console.log(eventId);
     const event = await Event.findById(eventId);
-    res.render("edit-event", { title: "Edit Event", event: event }); 
+    res.render("edit-event", { title: "Edit Event", event: event });
   } catch (error) {
     next(error);
   }
@@ -61,14 +65,14 @@ router.get("/event/:id/delete", async (req, res, next) => {
   }
 });
 
-router.get("/event/details/:id", (req, res, next)=>{
-  res.render("detailsEvent", {title: "Event Details"});
-   /* Event.findById(req.params.id)
-    .then((idDetails) =>{
-        res.render("detailsEvent", {idDetails});
+router.get("/event/details/:id", (req, res, next) => {
+  res.render("detailsEvent", { title: "Event Details" });
+  Event.findById(req.params.id)
+    .then((idDetails) => {
+      res.render("detailsEvent", { idDetails });
     })
-    .catch((error)=>{
-        next(error)
-    });*/
+    .catch((error) => {
+      next(error);
+    });
 });
 module.exports = router;
